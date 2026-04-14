3d000fda51d3676f38d5a2a9bee83c01e9dc234e
mlnq <mmstarkillerm@gmail.com>
2026-04-14 12:53:17 +0200

    add env attributes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import { loadEnv } from 'vite';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  site: 'https://mlnq.github.io',
-  base: '/resj-fest-astro',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const site = env.PUBLIC_SITE_URL || 'https://mlnq.github.io';
+  const base = env.PUBLIC_BASE_PATH || '/';
+
+  return {
+    integrations: [react()],
+    vite: {
+      plugins: [tailwindcss()],
+    },
+    site,
+    base,
+  };
 });
